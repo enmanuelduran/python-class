b6db9ccd86bb76ff57d1db1159a1a9fea7cd6b5e
blindv02 <blindv1@gmail.com>
2021-03-23 00:28:24 -0300

    adding validatos

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,21 +1,32 @@
 def players():
     players_dict = {
-        'player_x' : input('Add a name for player X: '),
-        'player_o' : input('Add a name for player O: '),
+        'player_x' : input('Add a name for player X:'),
+        'player_o' : input('Add a name for player O:'),
     }
     return players_dict
 
 def validator():
+    is_valid = False    
     players_dict = players()
 
-    if players_dict['player_x'] == players_dict['player_o']:
-        print("The players name can't be the same")
-        players_dict = players()
-    if players_dict['player_x'].isnumeric() or players_dict['player_o'].isnumeric():
-        print("The players name can't have numbers")
-        players_dict = players()
-    return players_dict
+    while not is_valid:
+        if not players_dict['player_o'] or not players_dict['player_x']:
+            print("The players name can't contain blanks")
+            players_dict = players()
+        elif ' ' in players_dict['player_x'] or ' ' in players_dict['player_o']:
+            print("The players name can't contain blanks")
+            players_dict = players()
+        elif players_dict['player_x'] == players_dict['player_o']:
+            print("The players name can't be the same")
+            players_dict = players()
+        elif players_dict['player_x'].isnumeric() or players_dict['player_o'].isnumeric():
+            print("The players name can't have just numbers")
+            players_dict = players()
+        else:
+            is_valid = True
 
+    return players_dict
+    
 def layout():
     x = ["_","_","_"]
     y = ["_","_","_"]
